a79491fb6cb8790fda50766881726f967e2204b8
Simon Kreuzer <mail@monsi.org>
2022-12-24 00:16:07 +0100

    changed csv handling - added file naming - changed start end page calculation

--- main.py
+++ main.py
@@ -37,17 +37,10 @@ def cli(separator, output_dir, file):
     output_dir.mkdir(parents=True, exist_ok=True)
 
     # Create with PDFs with starting pages from CSV
-    start_page = int(next(separator_pages)['page']) - 1
     for s in separator_pages:
         output = PdfWriter()
-        add_pages(output, input_pdf.pages[start_page:int(s['page']) - 1])
-        write(output_dir.joinpath(f'{start_page + 1}.pdf'), output)
-        start_page = int(s['page']) - 1
-
-    # Create last pdf
-    output = PdfWriter()
-    add_pages(output, input_pdf.pages[start_page:len(input_pdf.pages)])
-    write(output_dir.joinpath(f'{start_page + 1}.pdf'), output)
+        add_pages(output, input_pdf.pages[int(s['start']) - 1:int(s['end'])])
+        write(output_dir.joinpath('{}.pdf'.format(s['name'])), output)
 
     # Clean up
     file.close()
